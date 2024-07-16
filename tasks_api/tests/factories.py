00b58b4f8a7af6461dffc62ed47e6cd66e006303
AlexTraveylan <monsieurdemares@gmail.com>
2024-07-16 19:06:53 +0200

    chore: Add factories for Family, Member, PossibleTask, Task, and Invitation models in tests

---
+++ tasks_api/tests/factories.py
@@ -0,0 +1,49 @@
+from factory import LazyAttribute, SubFactory
+from factory.django import DjangoModelFactory
+
+from tasks_api.invitation.models import Invitation
+from tasks_api.member.models import Member
+from tasks_api.models import Family
+from tasks_api.possibles_task.models import PossibleTask
+from tasks_api.task.models import Task
+
+
+class FamilyFactory(DjangoModelFactory):
+    class Meta:
+        model = Family
+
+    family_name = LazyAttribute(lambda o: "Test Family")
+
+
+class MemberFactory(DjangoModelFactory):
+    class Meta:
+        model = Member
+
+    member_name = LazyAttribute(lambda o: "Test Member")
+    family = SubFactory(FamilyFactory)
+
+
+class PossibleTaskFactory(DjangoModelFactory):
+    class Meta:
+        model = PossibleTask
+
+    possible_task_name = LazyAttribute(lambda o: "Test Task")
+    description = LazyAttribute(lambda o: "Task description")
+    family = SubFactory(FamilyFactory)
+
+
+class TaskFactory(DjangoModelFactory):
+    class Meta:
+        model = Task
+
+    related_possible_task = SubFactory(PossibleTaskFactory)
+    member = SubFactory(MemberFactory)
+    duration_in_seconds = LazyAttribute(lambda o: 3600)
+
+
+class InvitationFactory(DjangoModelFactory):
+    class Meta:
+        model = Invitation
+
+    code = LazyAttribute(lambda o: "Test Code")
+    family = SubFactory(FamilyFactory)
