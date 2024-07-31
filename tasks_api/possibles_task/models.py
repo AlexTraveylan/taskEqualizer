b6b25072bfb18b03fa49ccb426cc678057f869f5
AlexTraveylan <monsieurdemares@gmail.com>
2024-07-31 21:56:17 +0200

    chore: Update family_id field in PossibleTask model

--- tasks_api/possibles_task/models.py
+++ tasks_api/possibles_task/models.py
@@ -25,7 +25,7 @@ class PossibleTask(models.Model):
             "description": self.description,
             "created_at": self.created_at,
             "updated_at": self.updated_at,
-            "family": self.family.id,
+            "family_id": self.family.id,
         }
 
     class Meta:
