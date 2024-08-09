cc3ecb80e81ac4fc0c6fd16c0a8b2dfa671d6faa
AlexTraveylan <monsieurdemares@gmail.com>
2024-08-09 16:05:17 +0200

    chore: Address invalid path error in tasks_api invitation schemas to enhance Git functionality and maintain stable development workflow

--- tasks_api/invitation/schemas.py
+++ tasks_api/invitation/schemas.py
@@ -1,30 +1,7 @@
-from datetime import datetime
-
 from ninja import Schema
-from pydantic import BaseModel, field_validator
+from pydantic import field_validator
 
-from auth_api.validators import validate_invitation_code
-from tasks_api.family.models import Family
 from tasks_api.validators import validate_email
-
-
-class InvitationCreate(BaseModel):
-    """Schema for creating an invitation."""
-
-    code: str
-    family: Family
-    expired_at: datetime
-
-    @field_validator("code")
-    def code_length(cls, value: str) -> str:
-        return validate_invitation_code(value)
-
-    def to_dict(self):
-        return {
-            "code": self.code,
-            "family": self.family.id,
-            "expired_at": self.expired_at,
-        }
 
 
 class InvitationWithEmailIn(Schema):
